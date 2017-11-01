a5d0039e3679c5956f5ac827f1f52bd64e095b69
Erison Silva <eerison@hotmail.com>
2017-11-01 11:12:23 -0300

    Add authentication and pages erros and fos_user
    - #9 create pages of erros in app/Resources/TwigBundle/views/Exception, all errors are explain in [Erros Pages](https://symfony.com/doc/current/controller/error_pages.html)
    - #5 add part functionality of authentication
    - add package fosUserBundle
    - add structure layout simple
    	- login in app/Resources/FosUserBundle/views/Security
    	- profile in app/Resources/FosUserBundle/views/Profile
    	- registration in app/Resources/FosUserBundle/views/Registration
    	- layout.html.twig

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
