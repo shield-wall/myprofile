b2a36b9890117e53a4ba2274139286fa412b0e73
Erison Silva <erison.sdn@gmail.com>
2018-10-13 09:20:20 -0300

    Added new enverioment QA

---
+++ web/app_qa.php
@@ -0,0 +1,18 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__.'/../vendor/autoload.php';
+if (PHP_VERSION_ID < 70000) {
+    include_once __DIR__ . '/../var/bootstrap.php.cache';
+}
+
+$kernel = new AppKernel('prod', false);
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
