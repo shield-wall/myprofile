84b8eea0c29ec8d49e135397c969be571477eb3d
Erison Silva <erison.sdn@gmail.com>
2019-11-24 09:18:44 -0300

    fixed cv

--- assets/js/curriculum_theme_01.js
+++ assets/js/curriculum_theme_01.js
@@ -1,5 +1,4 @@
-require('../css/global.scss');
-require('bootstrap');
+require('../bootstrap/css/bootstrap.css')
 require('@fortawesome/fontawesome-free/css/all.min.css');
 
 require('../css/curriculum/theme_01/styles.css');
