ad0d85da369614648ddbf8eac99b38d9ebe0d2c0
Erison Silva <erison.sdn@gmail.com>
2020-10-22 01:13:44 +0200

    remove jquery 1

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,17 +1,6 @@
-var $ = require('../js/jquery.js')
-global.$ = $;
-global.jQuery = $;
-
 require('../js/scripts.js')
-require('../js/jquery.stellar.min.js')
-require('../js/jquery.sticky.js')
-require('../js/wow.min.js')
-require('../js/jquery.countTo.js')
-require('../js/jquery.inview.min.js')
+require('../js/jquery.sticky.js') // need this to skill chart
+require('../js/jquery.inview.min.js') //need this to skill chart
 require('../js/jquery.easypiechart.js')
-require('../js/jquery.shuffle.min.js')
-require('../js/jquery.magnific-popup.min.js')
-require('../js/froogaloop2.min.js')
-require('../js/jquery.fitvids.js')
 
 require('../css/style.css')
