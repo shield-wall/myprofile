120774434a5c5f65c776973529e1c500e5791da4
Erison Silva <erison.sdn@gmail.com>
2020-11-14 15:55:34 +0100

    created page to reset password create one migration

--- src/Migrations/Version20201114145440.php
+++ src/Migrations/Version20201114145440.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201109191806 extends AbstractMigration
+final class Version20201114145440 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,9 +20,15 @@ final class Version20201109191806 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX uniq_957a647992fc23a8');
+        $this->addSql('CREATE SEQUENCE reset_password_request_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE reset_password_request (id INT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, expires_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_7CE748AA76ED395 ON reset_password_request (user_id)');
+        $this->addSql('COMMENT ON COLUMN reset_password_request.requested_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN reset_password_request.expires_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('DROP INDEX uniq_957a6479c05fb297');
         $this->addSql('DROP INDEX uniq_957a6479a0d96fbf');
+        $this->addSql('DROP INDEX uniq_957a647992fc23a8');
         $this->addSql('ALTER TABLE fos_user DROP username');
         $this->addSql('ALTER TABLE fos_user DROP username_canonical');
         $this->addSql('ALTER TABLE fos_user DROP email_canonical');
@@ -30,7 +36,7 @@ final class Version20201109191806 extends AbstractMigration
         $this->addSql('ALTER TABLE fos_user DROP confirmation_token');
         $this->addSql('ALTER TABLE fos_user DROP password_requested_at');
         $this->addSql('ALTER TABLE fos_user ALTER email TYPE VARCHAR(200)');
-        $this->addSql('ALTER TABLE fos_user ALTER roles TYPE JSON USING to_json(roles)');
+        $this->addSql('ALTER TABLE fos_user ALTER roles TYPE JSON');
         $this->addSql('ALTER TABLE fos_user ALTER roles DROP DEFAULT');
         $this->addSql('ALTER TABLE fos_user RENAME COLUMN enabled TO is_verified');
         $this->addSql('COMMENT ON COLUMN fos_user.roles IS NULL');
@@ -40,6 +46,8 @@ final class Version20201109191806 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE reset_password_request_id_seq CASCADE');
+        $this->addSql('DROP TABLE reset_password_request');
         $this->addSql('ALTER TABLE fos_user ADD username VARCHAR(180) NOT NULL');
         $this->addSql('ALTER TABLE fos_user ADD username_canonical VARCHAR(180) NOT NULL');
         $this->addSql('ALTER TABLE fos_user ADD email_canonical VARCHAR(180) NOT NULL');
@@ -51,8 +59,8 @@ final class Version20201109191806 extends AbstractMigration
         $this->addSql('ALTER TABLE fos_user ALTER roles DROP DEFAULT');
         $this->addSql('ALTER TABLE fos_user RENAME COLUMN is_verified TO enabled');
         $this->addSql('COMMENT ON COLUMN fos_user.roles IS \'(DC2Type:array)\'');
-        $this->addSql('CREATE UNIQUE INDEX uniq_957a647992fc23a8 ON fos_user (username_canonical)');
         $this->addSql('CREATE UNIQUE INDEX uniq_957a6479c05fb297 ON fos_user (confirmation_token)');
         $this->addSql('CREATE UNIQUE INDEX uniq_957a6479a0d96fbf ON fos_user (email_canonical)');
+        $this->addSql('CREATE UNIQUE INDEX uniq_957a647992fc23a8 ON fos_user (username_canonical)');
     }
 }
