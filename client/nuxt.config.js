3207553df008e100eed666eb62871d08dead0e57
Erison Silva <erison.sdn@gmail.com>
2021-06-18 19:57:22 +0200

    create ci to client

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -40,11 +40,14 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    '@nuxt/typescript-build'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxtjs/axios'],
+  modules: [
+    ['@nuxtjs/dotenv', { systemvars: true }],
+    '@nuxtjs/axios'
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
