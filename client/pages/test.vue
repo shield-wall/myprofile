6536a489ea7f85b1b17cb15411cbb83832bc037d
Erison Silva <erison.sdn@gmail.com>
2021-09-10 22:19:08 +0200

    Added daisyui - created small card to test the component

--- client/pages/test.vue
+++ client/pages/test.vue
@@ -7,14 +7,18 @@
 
       <div class="flex justify-center -mt-8">
         <img
-          src="https://bucket.myprofile.pro/myprofile-prod/users/e5d3a8a762db6fa77519789443a99c98/profile.webp"
-          class="rounded-full border-solid border-white border-2 -mt-3 w-24" />
+          src="https://bucket.myprofile.pro/myprofile-prod/users/70c52387c3bfd7ca5d628efda268b3f6/profile.webp"
+          class="-mt-3 w-24 mask mask mask-hexagon" />
       </div>
 
       <div class="text-white text-center font-sans px-3 pb-6 pt-2">
         <h3 class="text-2xl leading-8">Erison Silva</h3>
         <p class="mt-2">Senior PHP Developer</p>
       </div>
+    </div>
+    <a class="btn">Hello!</a>
+    <div class="alert alert-success">
+      Message sent successfully
     </div>
   </div>
 </template>
