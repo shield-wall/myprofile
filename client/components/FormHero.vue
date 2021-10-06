337e7bc446119bf3c80f78c0623c5f8d679dddbb
vicentepaiva <vicentepaivadev@gmail.com>
2021-10-06 11:30:07 -0300

    tasks ended issue #432

--- client/components/FormHero.vue
+++ client/components/FormHero.vue
@@ -2,19 +2,19 @@
     <div class="grid grid-cols-1 lg:grid-cols-2 lg:space-x-64 lg:p-32 sm:grid-cols-1">
 
 
-      <div class="flex-col justify-center hero-content lg:flex-row">
-          <div class="flex-grow">
-              <h1 class="text-white text-center text-5xl sm:text-5x1 mb-2">
+      <div class="flex-col hero-content lg:flex-row">
+          <div class="flex-grow align-items-center">
+              <h1 class="text-white text-center  text-5xl sm:text-5x1 mb-2 p-12">
                 Crie seu currículo em PDF e site pessoal em poucos minutos!
               </h1>
-                <p class="underline text-center text-blue-200 sm:text-lg p-5">
-                  Faça seu login para começar
-                </p>
+                <button class="btn btn-primary mt-6 ml-44 2xl:ml-52 lg:invisible md:visible sm:visible">
+                    Cadastrar
+                </button>
           </div>
       </div>
 
-        
-        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+         
+        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 lg:visible md:invisible sm:invisible">
             <div class="card-body">
               <div class="form-control">
                 <label class="label">
@@ -39,18 +39,12 @@
                   <span class="label-text">Password</span>
                 </label> 
                 <input placeholder="password" class="input input-bordered" type="password"> 
-                <label class="label">
-                  <a href="#" class="label-text-alt">Forgot password?</a>
-                </label>
               </div> 
               <div class="form-control">
                 <label class="label">
                   <span class="label-text">Confirm Password</span>
                 </label> 
                 <input placeholder="password" class="input input-bordered" type="password"> 
-                <label class="label">
-                  <a href="#" class="label-text-alt">Forgot password?</a>
-                </label>
               </div> 
               <div class="form-control mt-6">
                 <input type="button" value="Login" class="btn btn-primary">
