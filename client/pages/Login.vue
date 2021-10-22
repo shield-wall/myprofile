b92b5a81aed5e193195834d94473d4551727b055
Erison Silva <erison.sdn@gmail.com>
2021-10-22 21:04:17 +0200

    fix responsive login page

--- client/pages/Login.vue
+++ client/pages/Login.vue
@@ -1,40 +1,37 @@
 <template>
-  <div class="flex flex-col justify-center items-center min-h-screen bg-primary">
-    <NuxtLink to="/" class="text-center text-white text-4xl font-bold p-10 cursor-pointer">
-      My Profile
-    </NuxtLink>
-    <div class="flex items-center  min-w-min rounded-3xl shadow-2xl bg-base-100 justify-center mb-8">
-      <div class="mx-2">
-        <div>
-          <h1 class="text-3xl font-bold text-center mb-4 mt-4">
-            Login
-          </h1>
-        </div>
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Email</span>
-          </label>
-          <input placeholder="erison@gmail.com" class="input input-bordered" type="text">
-        </div>
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Senha</span>
-          </label>
-          <input placeholder="Sua_$enha" class="input input-bordered" type="password">
-        </div>
+  <div class="flex flex-col justify-center items-center min-h-screen bg-primary text-center">
+      <NuxtLink to="/" class="text-accent text-4xl font-bold m-10">
+        My Profile
+      </NuxtLink>
+      <div class="card bg-accent rounded-3xl shadow-2xl w-11/12 md:w-3/6 lg:w-80">
+        <div class="card-body">
 
-        <div class="form-control mt-6">
-          <input type="button" value="Entrar" class="btn btn-primary">
-        </div>
-        <div class="text-center w-80 py-4">
-          <NuxtLink to="/register" class="text-primary text-sm font-semibold underline m-4">
-            Registrar
-          </NuxtLink>
-          <NuxtLink to="/resetpassword" class="text-primary text-sm underline font-semibold">
-            Redefinir Senha
-          </NuxtLink>
+          <h1 class="text-3xl font-bold">Login</h1>
+          <form class="form-control">
+            <label class="label">
+              <span class="label-text">E-mail</span>
+            </label>
+            <input type="text" placeholder="Digite seu e-mail" class="input input-bordered">
+
+            <label class="label">
+              <span class="label-text">Senha</span>
+            </label>
+            <input type="password" placeholder="Digite sua senha" class="input input-bordered">
+
+            <input type="button" value="Entrar" class="btn btn-primary my-6">
+
+            <div class="text-center">
+              <NuxtLink to="/register" class="text-sm font-semibold">
+                Registrar
+              </NuxtLink>
+              <NuxtLink to="/resetpassword" class="text-sm font-semibold ml-6">
+                Redefinir Senha
+              </NuxtLink>
+            </div>
+          </form>
+
         </div>
       </div>
-    </div>
   </div>
+
 </template>
