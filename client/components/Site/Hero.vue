67568a7bed76a169f874f24cc190f607f642c922
LilianVasconcelos <lilian.ifce@gmail.com>
2021-10-25 19:30:55 +0200

    Update Hero.vue

--- client/components/Site/Hero.vue
+++ client/components/Site/Hero.vue
@@ -5,7 +5,7 @@
     <div class="flex">
       <div class="md:flex-1 lg:flex-1 space-y-9 text-center md:text-left lg:text-left">
         <h1 class="text-white text-3xl md:text-5xl lg:text-5xl">
-          Crie seu currículo em PDF e site pessoal em poucos minutos!
+          Crie seu currículo em PDF e site pessoal em poucos minutos 12333!
         </h1>
 
         <button class="btn btn-accent btn-wide lg:invisible md:invisible visible">
