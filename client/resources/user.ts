5978a92f1701200dc301aaa6b0200caf8eb2a469
Erison Silva <erison.sdn@gmail.com>
2021-11-21 14:38:30 +0100

    Activate lint rules

--- client/resources/user.ts
+++ client/resources/user.ts
@@ -2,10 +2,6 @@ import { Hydra, HydraCollection } from '~/resources/abstract.resource'
 import { ResourceInterface } from '~/resources/contracts/resource.interface'
 import { ResourceCollectionInterface } from '~/resources/contracts/resource.collection.interface'
 
-export class UserCollection extends HydraCollection implements ResourceCollectionInterface {
-  'hydra:member': User[];
-}
-
 export class User extends Hydra implements ResourceInterface {
   id: number|null = null
   firstName: string|null = null
@@ -14,4 +10,8 @@ export class User extends Hydra implements ResourceInterface {
   slug: string|null = null
   profileImage: string|null = null
   backgroundImage: string|null = null
+}
+
+export class UserCollection extends HydraCollection implements ResourceCollectionInterface {
+  'hydra:member': User[];
 }
