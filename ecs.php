8336ee1552c04109df103dd71a09f1ae79c13c5d
Erison Silva <erison.sdn@gmail.com>
2022-06-15 20:33:14 +0200

    Fix login and update dependencies

--- ecs.php
+++ ecs.php
@@ -5,26 +5,25 @@ declare(strict_types=1);
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (ECSConfig $ecsConfig): void {
     // A. full sets
-    $containerConfigurator->import(SetList::PSR_12);
-    $containerConfigurator->import(SetList::CLEAN_CODE);
-    $containerConfigurator->import(SetList::SYMFONY);
-    $containerConfigurator->import(SetList::SYMFONY_RISKY);
-    $containerConfigurator->import(SetList::DOCTRINE_ANNOTATIONS);
-    $containerConfigurator->import(SetList::SPACES);
+    $ecsConfig->import(SetList::PSR_12);
+    $ecsConfig->import(SetList::CLEAN_CODE);
+    $ecsConfig->import(SetList::DOCTRINE_ANNOTATIONS);
+    $ecsConfig->import(SetList::SPACES);
 
-    $parameters = $containerConfigurator->parameters();
+    $parameters = $ecsConfig->parameters();
     $parameters->set(Option::SKIP, [
         NotOperatorWithSuccessorSpaceFixer::class,
     ]);
 
 
     // B. standalone rule
-    $services = $containerConfigurator->services();
+    $services = $ecsConfig->services();
     $services->set(ArraySyntaxFixer::class)
         ->call('configure', [[
             'syntax' => 'short',
