b0521d527335edf2352889b252963001fcfe95bb
Erison Silva <erison.sdn@gmail.com>
2022-07-17 19:21:59 +0200

    Refactoring tests to use pest

--- tests/Unit/Service/ProfileImageServiceTest.php
+++ tests/Unit/Service/ProfileImageServiceTest.php
@@ -4,51 +4,42 @@ namespace App\Tests\Unit\Service;
 
 use App\Entity\User;
 use App\Service\ProfileImageService;
-use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use transloadit\Transloadit;
 
-class ProfileImageServiceTest extends TestCase
-{
-    /**
-     * @test
-     * @testdox it is passing always the path to transloadit configuration.
-     * @testWith ["https://bucket.foo.bar/prefix/image/path/fake.png"]
-     *          ["https://cdn.foo.bar/prefix/image/path/fake.png"]
-     *          ["/image/path/fake.png"]
-     */
-    public function passPathToConfiguration(string $getProfileImageUrl)
-    {
-        // Mocks
-        $paramMock = $this->createMock(ParameterBagInterface::class);
-        $paramMock
-            ->method('get')
-            ->willReturn(1);
+it('is passing always the path to transloadit configuration', function (string $getProfileImageUrl) {
+    // Mocks
+    $paramMock = mock(ParameterBagInterface::class)->expect(
+        get: fn ($name) => 1,
+    );
 
-        $transloaditMock = $this->createMock(Transloadit::class);
-        $transloaditMock
-            ->method('createAssembly')
-            ->with($this->callback(function(array $configs) {
-                return $configs['params']['steps']['export']['path'] === '/image/path/fake.png'
-                    && $configs['params']['steps']['export']['url_prefix'] === 'https://cdn.foo.bar/prefix/';
-            }));
+    $transloaditMock = $this->createMock(Transloadit::class);
+    $transloaditMock
+        ->method('createAssembly')
+        ->with($this->callback(function(array $configs) {
+            return $configs['params']['steps']['export']['path'] === '/image/path/fake.png'
+                && $configs['params']['steps']['export']['url_prefix'] === 'https://cdn.foo.bar/prefix/';
+        }));
 
-        $userMock = $this->createMock(User::class);
-        $userMock
-            ->expects($this->once())
-            ->method('getProfileImage')
-            ->willReturn($getProfileImageUrl);
+    $userMock = mock(User::class)
+        ->shouldReceive('getProfileImage')
+        ->andReturn($getProfileImageUrl)
+        ->once()
+        ->getMock();
 
-        $fileMock = $this->createMock(UploadedFile::class);
+    $fileMock = $this->createMock(UploadedFile::class);
 
-        $profileImageServiceMock = $this
-            ->getMockBuilder(ProfileImageService::class)
-            ->setConstructorArgs([$transloaditMock, $paramMock, 'https://cdn.foo.bar/prefix', 'https://bucket.foo.bar/prefix'])
-            ->onlyMethods(['removeFile'])
-            ->getMock();
+    $profileImageServiceMock = $this
+        ->getMockBuilder(ProfileImageService::class)
+        ->setConstructorArgs([$transloaditMock, $paramMock, 'https://cdn.foo.bar/prefix', 'https://bucket.foo.bar/prefix'])
+        ->onlyMethods(['removeFile'])
+        ->getMock();
 
-        // Run code
-        $profileImageServiceMock->upload($userMock, $fileMock);
-    }
-}
+    // Run code
+    $profileImageServiceMock->upload($userMock, $fileMock);
+})->with([
+    ["https://bucket.foo.bar/prefix/image/path/fake.png"],
+    ["https://cdn.foo.bar/prefix/image/path/fake.png"],
+    ["/image/path/fake.png"],
+]);
