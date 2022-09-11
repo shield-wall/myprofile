90d864ce9898000e98c6c7fc0db3c467786a7ec1
Erison Silva <erison.sdn@gmail.com>
2022-09-11 15:34:29 +0200

    Test Deploy 2

--- src/Controller/Site/SiteController.php
+++ src/Controller/Site/SiteController.php
@@ -17,6 +17,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 #[Route(name: 'app_')]
 class SiteController extends AbstractController
 {
