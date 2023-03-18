2cde89a38c56ae53493a1df871a553f9825bbe81
Erison Silva <erison.sdn@gmail.com>
2023-03-18 12:41:42 +0100

    fixed pdf generation

--- node/pdf.ts
+++ node/pdf.ts
@@ -1,19 +1,52 @@
-import puppeteer from 'puppeteer';
+import puppeteer, {Page} from 'puppeteer';
+import fs from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            '--disable-web-security',
+            '--allow-file-access-from-files',
+            '--allow-file-access',
+            '--allow-cross-origin-auth-prompt',
+        ],
+    });
     const page = await browser.newPage();
 
-    await page.goto('http://localhost:3000/');
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    await getPage(page, __dirname);
 
     // Set screen size
     await page.setViewport({width: 1080, height: 1024});
 
-    await page.pdf({
+    let pdfFolder = `${__dirname}/../data/pdf`;
+    if (!fs.existsSync(pdfFolder)) {
+        fs.mkdirSync(pdfFolder, { recursive: true });
+    }
+
+    let pdf = await page.pdf({
         format: 'A4',
-        path: `my-fance-invoice.pdf`,
-        preferCSSPageSize: true,
+        // preferCSSPageSize: true,
+        path: `${pdfFolder}/resume.pdf`,
+        printBackground: true,
     })
 
     await browser.close();
+
+    return pdf;
 })();
+
+function getPage(page: Page, __dirname: string) {
+    if (process.argv[2] && process.argv[2] === '--file=true') {
+        const html = fs.readFileSync(`${__dirname}/../dist/index.html`, 'utf8')
+        return  page.setContent(html, {
+            waitUntil: 'domcontentloaded',
+        })
+    }
+
+    return page.goto('http://localhost:3000/');
+}
