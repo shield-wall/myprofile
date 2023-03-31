2df02e099a1912261d31ac1e2e0737477ce5d8df
Erison Silva <erison.sdn@gmail.com>
2023-03-31 20:27:44 +0200

    dark mode as default

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
     title: 'My profile',
     tagline: 'Create your resume easily from github.',
-    favicon: 'img/favicon.ico',
+    favicon: 'img/logo_dark.svg',
     
     // Set the production url of your site here
     url: 'https://myprofile.pro',
