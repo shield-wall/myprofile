3ef7b38c472a97097a27c7f396fd41e89c4792e1
Erison Silva <erison.sdn@gmail.com>
2023-04-07 08:43:53 +0200

    Fix redirect issues on docs (#759)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
-    
+    trailingSlash: true,
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'shield-wall', // Usually your GitHub org/user name.
