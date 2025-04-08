c7773ca4735c72e7b3b2ac64e565ec44bbcb905e
Erison Silva <hey@erison.work>
2025-04-08 18:55:36 +0200

    Upgrade puppeter and vite
    puppeter version 26.6.0
    vite 6.2.5
    
    Issue #812

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dns from 'node:dns'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [basicSsl({
+      /** name of certification */
+      name: 'test',
+      /** custom trust domains */
+      domains: ['*.custom.com'],
+      /** custom certification directory */
+      certDir: '/app/.devServer/cert',
+  })],
 	base: "./",
     assetsInclude: ['**/*.md'],
     build: {
